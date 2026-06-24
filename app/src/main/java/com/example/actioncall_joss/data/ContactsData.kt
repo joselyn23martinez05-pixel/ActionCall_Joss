40484636707d4df4bb4f0c9edfd1959b1fa8cda5
joselyn23martinez05 <joselyn23martinez05@gmail.com>
2026-06-23 23:28:19 -0600

    Commit 6 Creación de Lista en Data

--- app/src/main/java/com/example/actioncall_joss/data/ContactsData.kt
+++ app/src/main/java/com/example/actioncall_joss/data/ContactsData.kt
@@ -1,2 +1,15 @@
 package com.example.actioncall_joss.data
 
+import com.example.actioncall_joss.model.Contacto
+
+val listaContactos = listOf(
+
+    Contacto("Ana López", "4771234567", 0),
+    Contacto("Juan Pérez", "4772345678", 0),
+    Contacto("María García", "4773456789", 0),
+    Contacto("Carlos Ruiz", "4774567890", 0),
+    Contacto("Laura Hernández", "4775678901", 0),
+    Contacto("Pedro Sánchez", "4776789012", 0),
+    Contacto("Sofía Martínez", "4777890123", 0),
+    Contacto("Diego Torres", "4778901234", 0)
+)
